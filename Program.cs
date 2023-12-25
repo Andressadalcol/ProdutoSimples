4c163e03e5d8bff9a7f7e3bd41c0696661c3ae24
Andressa Dal Col <104804693+Andressadalcol@users.noreply.github.com>
2023-12-25 18:06:56 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vl1, vl2, prod;
+
+            
+            Console.WriteLine("Valor 1: ");
+            vl1= int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 2: ");
+            vl2 = int.Parse(Console.ReadLine());
+
+            prod = vl1 * vl2;
+
+            Console.WriteLine("Prod = " + prod);
+            Console.ReadLine();
+
+        }
+    }
+}
